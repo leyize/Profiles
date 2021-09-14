2f26dd9116632265f7c0ae6df6ac11404b35f44f
leyize <leyize@users.noreply.github.com>
2021-09-15 00:31:06 +0800

    Update Location.js

--- QuantumultX/Location.js
+++ QuantumultX/Location.js
@@ -4,7 +4,7 @@ if ($response.statusCode != 200) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = '𝗤𝘂𝗮𝗻𝘁𝘂𝗺𝘂𝗹𝘁 𝗫';
+var title = obj['country'];
 var subtitle = '\n';
 var ip = obj['query'];
 var description = obj['country'] + '\n' + obj['city'] + '\n' + obj['isp'] + '\n' + obj['ipType'];
