30dc1b7ae12bb7cc3804f6b8d231fef98ff57f95
leyize <leyize@users.noreply.github.com>
2021-09-17 14:36:06 +0800

    Update Location.js

--- QuantumultX/Location.js
+++ QuantumultX/Location.js
@@ -4,7 +4,7 @@ if ($response.statusCode != 200) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = '海有舟可渡 / 山有路可行';
+var title = '海可渡山可行';
 var subtitle = '\n';
 var ip = obj['query'];
 var description = obj['country'] + '\n' + obj['city'] + '\n' + obj['isp'] + '\n' + obj['ipType'];
